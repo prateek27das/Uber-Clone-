fcd71aadee08241a0199671c7082922eabef9dd6
prateek27das <prateek17das@gmail.com>
2021-03-02 00:30:43 +0600

    Uber Clone

---
+++ app/src/main/java/com/example/uber/Common.kt
@@ -0,0 +1,5 @@
+package com.example.uber
+
+object Common {
+    val DRIVER_INFO_REFERENCE: String = "DriverInfo"
+}
---
+++ app/src/main/java/com/example/uber/Model/DriverInfoModel.kt
@@ -0,0 +1,8 @@
+package com.example.uber.Model
+
+class DriverInfoModel {
+     var firstName:String=""
+     var lastName:String=""
+     var phoneNumber:String=""
+     var rating = 0.0
+}
